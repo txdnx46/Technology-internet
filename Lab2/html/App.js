edc225964033a966481e05aeaf47c9aca43e2746
TanaratSudjai <650112418012@bru.ac.th>
2023-11-16 15:45:27 +0700

    commit-lab2

---
+++ Lab2/html/App.js
@@ -0,0 +1,22 @@
+/* var num = 10 
+     let name = "Tanarat Sudjai"
+     age = 20 
+     arr = ["java","html","css"]
+     obj={name:"Tanrat",age:20,skils:"java"}
+     data = {adress:["104","Mega",15], position: "Dev"}
+     console.log(obj.name, data.position, "Language skils ", arr[0])
+     console.log(data.adress[1])*/
+    //document.getElementById("msg").innerHTML = obj.name //+HTML
+    //let longtxt = obj.skils[2] + " : " + obj.name + " position " + data.position 
+
+    data = { adress: ["104", "Mega", 15], position: "Dev" }
+    obj = { name: "Tanrat Sudjai", age: 20, skils: ["java", "php", "javasclip"] }
+
+    longtxt =  `Skill language programming ${obj.skils[2]} 
+                Name : ${obj.name} 
+                Position : ${data.position}`
+
+    //run j query
+    $(function(){
+        $("#longtxt").html(longtxt)
+    })
